5dcfc846533d17f0d690f301b3e8429b09082b65
Ravi <dev.raviraghav93@gmail.com>
2023-03-25 03:13:27 +0530

    Init commit
    Init commit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using KafkaConsumer;
+
+Console.WriteLine("Hello!, This is Kafka Consumer Application");
+ConsumeMessage consumeMessage = new ConsumeMessage();
+consumeMessage.ReadMessage();
